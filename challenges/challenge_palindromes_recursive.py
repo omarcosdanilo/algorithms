293b2e24b53a0afed41987eb839842cf0a8a97fa
Marcos Danilo <marcosdanilo94@gmail.com>
2022-11-19 19:03:12 -0300

    feat: cria a função is_palindrome_recursive
    a função utiliza um algoritmo recursivo para verificar se uma string passada por parâmetro é um palíndromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    if word is None or len(word) == 0:
+        print(low_index, high_index)
+        return False
+
+    if len(word) == 1 or (len(word) == 2 and word[0] == word[1]):
+        return True
+
+    if word[0] == word[-1]:
+        return is_palindrome_recursive(word[1:-1], 1, 1)
+    else:
+        return False
