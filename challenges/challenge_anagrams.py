d671c0ac4fd7d5c7fa0332b35564b48fc03fc274
Marcos Danilo <marcosdanilo94@gmail.com>
2022-11-22 19:30:03 -0300

    feat: cria a função is_anagram
    a função recebe duas string e retorna uma tupla de 3 elementos, as duas strings ordenadas em ordem alfabética e um booleano que é True caso as strings sejam anagramas ou False caso as strings não sejam anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,25 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string_ordered = "".join(quick_sort(first_string.lower()))
+    second_string_ordered = "".join(quick_sort(second_string.lower()))
+
+    is_anagram = first_string_ordered == second_string_ordered
+
+    if first_string == '' or second_string == '':
+        is_anagram = False
+
+    return (
+        first_string_ordered,
+        second_string_ordered,
+        is_anagram
+    )
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        return (
+            quick_sort([x for x in arr[1:] if x < arr[0]])
+            + [arr[0]]
+            + quick_sort([x for x in arr[1:] if x >= arr[0]])
+        )
