ee08892136b52d0cf0721d756fd1924018fd5cd2
Marcos Danilo <marcosdanilo94@gmail.com>
2022-11-21 21:01:04 -0300

    feat: cria a função study_schedule
    a função recebe uma lista com tuplas contendo um horario de entrada e saída de estudantes e retorna quantos estudantes estão estudando de acordo com o target_time passado por parâmetro

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    counter = 0
+
+    for time in permanence_period:
+        if (type(time[0]) is not int or type(time[1]) is not int):
+            return None
+
+        elif target_time in range(time[0], time[1] + 1):
+            counter += 1
+
+    return counter
