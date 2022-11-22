73f1bf4312b9b6392765817e253e2aa53b8696bb
Marcos Danilo <marcosdanilo94@gmail.com>
2022-11-21 21:19:33 -0300

    refactor: refatora o algoritmo da função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    if target_time is None:
-        return None
-
     counter = 0
-
-    for time in permanence_period:
-        if (type(time[0]) is not int or type(time[1]) is not int):
-            return None
-
-        elif target_time in range(time[0], time[1] + 1):
-            counter += 1
+    try:
+        for time in permanence_period:
+            if target_time >= time[0] and target_time <= time[1]:
+                counter += 1
+    except TypeError:
+        return None
 
     return counter
