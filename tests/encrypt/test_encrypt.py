d4446b9137ed1ba62a6466c9cc7614c8a63583b8
Marcos Danilo <marcosdanilo94@gmail.com>
2022-11-21 21:59:29 -0300

    test: garante que a função encrypt_message funciona corretamente

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("AABBCC", 3) == "BAA_CCB"
+    assert encrypt_message("ABBCCA", 4) == "AC_CBBA"
+    assert encrypt_message("AABBCC", -1) == "CCBBAA"
+
+    with pytest.raises(TypeError) as exception:
+        encrypt_message("AABBCC", "key_invalida")
+    assert exception.value.args[0] == "tipo inválido para key"
+    assert exception.type == TypeError
+
+    with pytest.raises(TypeError) as exception:
+        encrypt_message(123456, 3)
+    assert exception.value.args[0] == "tipo inválido para message"
+    assert exception.type == TypeError
